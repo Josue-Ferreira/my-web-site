7abf0ac2e4766cd821690f70648199685702b801
Ferreira Josué <ferreira.josu@gmail.com>
2023-05-19 00:44:51 +0200

    first section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,83 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import styled from 'styled-components';
+import {AiOutlineArrowDown} from 'react-icons/ai';
+import NavBar from './components/NavBar';
+import photoProfil from './assets/photo_profil.png';
+import github from './assets/github.png';
+import linkedin from './assets/linkedin.png';
+
+const IntroductionSection = styled.section`
+  background: linear-gradient(to top, #bdc3c7, #2c3e50);
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const Welcome = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const ImgPhotoProfil = styled.img`
+  height: 70vh;
+`;
+
+const Name = styled.strong`
+  color: #F1DB66;
+`;
+
+const Links = styled.div`
+  display: flex;
+  align-items: center;
+  width: 90%;
+  margin: auto;
+`;
+
+const ImgLink = styled.img`
+  height: 5vh;
+  margin-right: 10px;
+`;
+
+const Button = styled.a`
+  display: inline-block;
+  text-decoration: none;
+  background-color: #F1DB66;
+  border-radius: 40px;
+  padding: 5px 5vw;
+  margin: 5vh 0;
+  color: #2C3E50;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [language, setLanguage] = useState('en');
+  const [page, setPage] = useState('home');
 
   return (
-    <div></div>
+    <div>
+      <IntroductionSection>
+        <NavBar page={page} setPage={setPage} language={language} setLanguage={setLanguage} />
+        <Welcome>
+          <div>
+            <h1>Hi ! I'm <Name>Josué Ferreira</Name></h1>
+            <h2>Web Developper</h2>
+            <Button href="../public/CV.pdf" download>Download CV</Button>
+          </div>
+          <ImgPhotoProfil src={photoProfil} alt="Josué Ferreira photo" title="Josué Ferreira"/>
+        </Welcome>
+        <Links>
+          <a href="https://github.com/Josue-Ferreira" target="_blank">
+            <ImgLink src={github} alt="GitHub link" title='Link to GitHub'/>
+          </a>
+          <a href="https://www.linkedin.com/in/josué-ferreira-694095276" target="_blank">
+            <ImgLink src={linkedin} alt="LinkedIn link" title='Link to LinkedIn' />
+          </a>
+          <AiOutlineArrowDown id='arrow' style={{flex: '1', fontSize: '3em'}} />
+        </Links>
+      </IntroductionSection>
+    </div>
   )
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import { useState } from 'react';
+import { Nav, NavItem, NavLink } from 'reactstrap';
+import logo from '../assets/logo1.svg';
+import styled from 'styled-components';
+
+const Img = styled.img`
+    height: 10vh;
+    width: auto;
+`;
+
+const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center; 
+    margin: 0 10px;
+`;
+
+const Button = styled.button`
+  background: rgba(0,0,0,0);
+  border-radius: 40px;
+  padding: 5px;
+  margin: 0 5px;
+  color: #F1DB66;
+  transition: border 0.1s linear;
+`;
+
+const NavBar = ({page, setPage, language, setLanguage}) => {
+    return (
+        <Container>
+            <Img src={logo} alt="" />
+            <Nav pills>
+                <NavItem>
+                    <NavLink 
+                        href="#" 
+                        className={page === 'home' ? 'active' : ''} 
+                        style={page === 'home' ? {backgroundColor: '#F1DB66'} : {color: '#bdc3c7'}}
+                        onClick={() => setPage('home')}
+                        >
+                            HOME
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink 
+                        href="#" 
+                        className={page === 'aboutme' ? 'active' : ''} 
+                        style={page === 'aboutme' ? {backgroundColor: '#F1DB66'} : {color: '#bdc3c7'}}
+                        onClick={() => setPage('aboutme')}
+                        >
+                            ABOUT ME
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink 
+                        href="#" 
+                        className={page === 'projects' ? 'active' : ''} 
+                        style={page === 'projects' ? {backgroundColor: '#F1DB66'} : {color: '#bdc3c7'}}
+                        onClick={() => setPage('projects')}
+                        >
+                            PROJECTS
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink 
+                        href="#" 
+                        className={page === 'hobbies' ? 'active' : ''} 
+                        style={page === 'hobbies' ? {backgroundColor: '#F1DB66'} : {color: '#bdc3c7'}}
+                        onClick={() => setPage('hobbies')}
+                        >
+                            HOBBIES
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink 
+                        href="#" 
+                        className={page === 'contactme' ? 'active' : ''} 
+                        style={page === 'contactme' ? {backgroundColor: '#F1DB66'} : {color: '#bdc3c7'}}
+                        onClick={() => setPage('contactme')}
+                        >
+                            CONTACT ME
+                    </NavLink>
+                </NavItem>
+            </Nav>
+            <Container>
+                <Button style={language === 'en' ? {border: 'solid'} : {border: 'none'}} onClick={() => setLanguage('en')}>EN</Button>
+                <Button style={language === 'fr' ? {border: 'solid'} : {border: 'none'}} onClick={() => setLanguage('fr')}>FR</Button>
+            </Container>
+        </Container>
+    );
+};
+
+export default NavBar;
