2d23da41704b20cb05da95c7f8a7b85a4b7871dc
Ferreira Josué <ferreira.josu@gmail.com>
2023-06-01 15:15:34 +0200

    modifyed contactme page and added text contactme language in json

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -4,6 +4,9 @@ import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import Navigation from '../components/Navigation';
 import linkedin from '../assets/logos/linkedin.png';
+import {BsTelephoneFill} from 'react-icons/bs'
+import {MdEmail} from 'react-icons/md'
+import textLang from '../assets/text-lang.json';
 
 const Section = styled.section`
   background: linear-gradient(to top, rgba(189,195,199,0), #2c3e50); //#bdc3c7
@@ -14,53 +17,59 @@ const Section = styled.section`
 
 const Contact = styled.div`
     height: 70vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+    width: 30vw;
+    margin: 0 auto;
+    display: grid;
+    grid-template: repeat(4, 100px)/repeat(2, 1fr);
     align-items: center;
 `;
 
 const Title = styled.h2`
     color: #F1DB66;
-    margin: 10vh 0;
+    margin: 5vh 0;
+    grid-column: 1/3;
+    text-align: center;
 `;
 
 const ImgLink = styled.img`
-  height: 5vh;
-  margin-right: 10px;
+  height: 30px;
 `;
 
 const Button = styled.a`
-  display: inline-block;
+  justify-self: end;
+  display: flex;
   text-decoration: none;
   background-color: #F1DB66;
-  border-radius: 40px;
-  padding: 5px 5vw;
-  margin: 5vh 0;
-  color: #2C3E50;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  margin: 20px;
+  justify-content: center;
+  align-items: center;
+  /* margin: 5vh 0; */
+  color: inherit;
+  &:hover{
+    color: inherit; 
+  }
 `;
 
 const ContactMe = () => {
-    const {page, lang} = useParams();
+    const {lang} = useParams();
     const [language, setLanguage] = useState(lang ? lang : 'en');
 
     return (
         <Section>
             <Navigation page={'contactme'} language={language} setLanguage={setLanguage} />
             <Contact>
-                <Title>Contact Details</Title>
-                <ul style={{listStyleType: 'none'}}>
-                    <li>Phone Number : +33 6 63 85 39 84</li>
-                    <li>
-                        <p style={{display: 'inline-block', marginRight: '10px'}}>Send me a message on LinkedIn</p>
-                        <a href="https://www.linkedin.com/in/josué-ferreira-694095276" target="_blank">
-                            <ImgLink src={linkedin} alt="LinkedIn link" title='Link to LinkedIn' />
-                        </a>
-                    </li>
-                    <li>
-                        <Button href="mailto:ferreira.josu@gmail.com">Send me an email</Button>
-                    </li>
-                </ul>
+                <Title>{textLang[language].pages.contactme.title}</Title>
+                <Button href="tel:+33663853984"><BsTelephoneFill /></Button>
+                <div>+33 6 63 85 39 84</div>
+                <Button href="https://www.linkedin.com/in/josué-ferreira-694095276" target="_blank">
+                    <ImgLink src={linkedin} alt="LinkedIn link" title='Link to LinkedIn' />
+                </Button>
+                <div>{textLang[language].pages.contactme.linkedIn}</div>
+                <Button href="mailto:ferreira.josu@gmail.com"><MdEmail/></Button>
+                <div>{textLang[language].pages.contactme.mail}</div>
             </Contact>
         </Section>
     );
